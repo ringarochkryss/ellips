b73c65edba217a034e8b3fcd2e8ad71d43fac22a
Petra  Mellbrand <petra@mellbrand.com>
2020-01-27 22:06:35 +0000

    Add settings to admin.py
    Add categories and Products on /admin

--- .theia/citywalls/shop/admin.py
+++ .theia/citywalls/shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','artist']
+    prepopulated_fields = {'artist':('name',)}
+admin.site.register(Category,CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','price','stock','available','created','updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'artist':('name',)}
+    list_per_page = 20
+admin.site.register(Product,ProductAdmin)
