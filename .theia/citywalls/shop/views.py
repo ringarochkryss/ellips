fe5fb40b0f5c483c137c5638a89615c54205098a
Petra  Mellbrand <petra@mellbrand.com>
2020-01-27 20:43:17 +0000

    Create a starting page
    Start page for the app store

--- .theia/citywalls/shop/views.py
+++ .theia/citywalls/shop/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    text_var = 'This is the Mellbrand shop. '
+    return HttpResponse(text_var)
